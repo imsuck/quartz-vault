a64b1b4f8f60f63ac5ee665198609cc8d0034330
imsuck <imsuck12@gmail.com>
2026-04-20 18:55:35 +0700

    Quartz sync: Apr 20, 2026, 6:55 PM

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ CACHE_DIR=node_modules/.cache/quartz
 
 if [ -d "$CACHE_DIR" ]; then
   rm -rf .quartz
-  mv "$CACHE_DIR" .quartz
+  # mv "$CACHE_DIR" .quartz
 fi
 
 if npx quartz plugin install && npx quartz build; then
   rm -rf "$CACHE_DIR"
-  [ -d .quartz ] && mv .quartz "$CACHE_DIR"
+  # [ -d .quartz ] && mv .quartz "$CACHE_DIR"
 else
   echo "build failed, keeping old cache"
 fi
