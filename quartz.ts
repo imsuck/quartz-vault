0bd9196161e5442b1b3c63366fcee5b7e17d6d9d
imsuck <imsuck12@gmail.com>
2026-05-25 14:27:48 +0700

    add font awesome icons for explorer

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,21 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
+import * as ExternalPlugin from "./.quartz/plugins";
+import type { ExplorerOptions } from "./.quartz/plugins";
+
+const explorerPrefixes: ExplorerOptions["mapFn"] = (node) => {
+  if (node.isFolder) {
+    node.displayName = ` ${node.displayName}`;
+  } else {
+    node.displayName = ` ${node.displayName}`;
+  }
+  return node;
+}
+
+ExternalPlugin.Explorer({
+  mapFn: explorerPrefixes,
+});
+
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
