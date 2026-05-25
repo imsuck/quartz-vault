cd6d2bc6f0a41d890332dd31c4c4c3529c0e1845
imsuck <imsuck12@gmail.com>
2026-05-25 14:32:02 +0700

    format files

--- quartz.ts
+++ quartz.ts
@@ -1,20 +1,20 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
-import * as ExternalPlugin from "./.quartz/plugins";
-import type { ExplorerOptions } from "./.quartz/plugins";
+import * as ExternalPlugin from "./.quartz/plugins"
+import type { ExplorerOptions } from "./.quartz/plugins"
 
 const explorerPrefixes: ExplorerOptions["mapFn"] = (node) => {
   if (node.isFolder) {
-    node.displayName = ` ${node.displayName}`;
+    node.displayName = ` ${node.displayName}`
   } else {
-    node.displayName = ` ${node.displayName}`;
+    node.displayName = ` ${node.displayName}`
   }
-  return node;
+  return node
 }
 
 ExternalPlugin.Explorer({
   mapFn: explorerPrefixes,
-});
+})
 
 const config = await loadQuartzConfig()
 export default config
