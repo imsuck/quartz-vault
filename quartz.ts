9b87b79d9b1216089309acc9933e2a4468957940
imsuck <imsuck12@gmail.com>
2026-05-27 18:02:17 +0700

    random page titles

--- quartz.ts
+++ quartz.ts
@@ -14,6 +14,42 @@ ExternalPlugin.Explorer({
   mapFn: explorerPrefixes,
 })
 
-const config = await loadQuartzConfig()
+// author: https://github.com/fanteastick
+const possiblePageTitles = [
+  "(｡•ㅅ•｡)~✧",
+  "૭( ᵕ•̀ᵕ•́૭)",
+  "(૭ •́ ᵕ•̀ )૭",
+  "(๑>؂·̀๑)",
+  "৻(•̀ᗜ•́৻)",
+  "٩(•̤̀ᵕ•̤́๑)",
+  "(｡•́︿•̀｡)",
+  "ᕙ( •̀ ᗜ •́ )ᕗ",
+  "(๑•́ ₃ •̀๑)",
+  "(づ ̄ ³ ̄)づ",
+  "( ˵ •̀ ᴗ •́˵)",
+  "(๑•́o•̀๑)",
+  "٩(๑❛ᴗ❛๑)6",
+  "(╥﹏╥)",
+  "( ˘ ³˘(◡‿◡˶)",
+  "٩(๑˘•ω•˘๑)٩",
+  "૮ ˶ᵔ ᵕ ᵔ˶ ა",
+  "(˶˃ ᵕ ˂˶).ᐟ",
+  "ദ്ദി •⩊• )",
+  "꒰ᐢ. .ᐢ꒱₊˚⊹",
+  "˚ʚ♡ɞ˚",
+  "⸜(｡˃ ᵕ ˂ )⸝♡",
+  "`⎚⩊⎚´ -✧",
+  "(˶˃ ᵕ ˂˶)~✧",
+  "(˵•̀ ᴗ •́˵)~✧",
+  "(૮ ᵕ•̀ )૮~✧",
+  "一人一つ",
+];
+function getRandomPageTitle(): string {
+  return possiblePageTitles[Math.floor(Math.random() * possiblePageTitles.length)];
+}
+
+const config = await loadQuartzConfig({
+  pageTitle: getRandomPageTitle(),
+})
 export default config
 export const layout = await loadQuartzLayout()
